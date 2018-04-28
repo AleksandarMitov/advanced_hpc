d36da15835cb5bea1f0ce44bb28d39975ea9f54d
Aleksandar Mitov <AleksandarMitov@users.noreply.github.com>
2018-04-28 22:34:09 +0100

    Add files via upload

--- d2q9-bgk_gpu.c
+++ d2q9-bgk_gpu.c
@@ -716,7 +716,7 @@ float merged_timestep_ops(const t_param params, t_speed_arrays*restrict cells, t
   const float w2 = 1.f / 36.f; /* weighting factor */
   float tot_u = 0.f;         /* accumulated magnitudes of velocity for each cell */
   /* loop over _all_ cells */
-  #pragma omp target enter data map(to: a[0:N], b[0:N],c[0:N],d[0:N],e[0:N],f[0:N],g[0:N]) \
+  #pragma omp target enter data map(to: a[0:N], b[0:N],c[0:N],d[0:N],e[0:N],f[0:N],g[0:N], \
     h[0:N],i[0:N],at[0:N],bt[0:N],ct[0:N],dt[0:N],et[0:N],ft[0:N],gt[0:N],ht[0:N],it[0:N])
   for (int jj = 0; jj < params.ny; jj++)
   {
@@ -984,7 +984,7 @@ float merged_timestep_ops(const t_param params, t_speed_arrays*restrict cells, t
 
     }
   }
-  #pragma omp target exit data map(from: a[0:N], b[0:N],c[0:N],d[0:N],e[0:N],f[0:N],g[0:N]) \
+  #pragma omp target exit data map(from: a[0:N], b[0:N],c[0:N],d[0:N],e[0:N],f[0:N],g[0:N], \
     h[0:N],i[0:N],at[0:N],bt[0:N],ct[0:N],dt[0:N],et[0:N],ft[0:N],gt[0:N],ht[0:N],it[0:N])
   return tot_u;
 }
