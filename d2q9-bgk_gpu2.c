32575834b721d45640d1448ea5c31699a18f13f4
Aleksandar Mitov <AleksandarMitov@users.noreply.github.com>
2018-05-02 14:30:19 +0100

    Add files via upload

--- d2q9-bgk_gpu2.c
+++ d2q9-bgk_gpu2.c
@@ -1273,7 +1273,7 @@ float av_velocity(const t_param params, int* obstacles, int flag, float* tot_u_b
       }
     }
   }
-  #pragma omp target update from(tot_u_buffer[0:1])
+  #pragma omp target update from(tot_u_buffer[0:1], tot_u)
   {}
   //tot_u = tot_u_buffer[0];
   //return tot_u / (float)tot_cells;
